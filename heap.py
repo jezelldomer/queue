0947bb54f60dfa903be8de4a1b8a3ac906614ca1
jezelldomer <jzlldomer@gmail.com>
2022-12-12 21:45:59 +0800

    using heappop

--- heap.py
+++ heap.py
@@ -6,3 +6,11 @@ heappush(fruits, "apple")
 heappush(fruits, "banana")
 
 print(fruits)
+
+print("\n--___•___--___•___--___•___--___•___--___•___--\n")
+
+from heapq import heappop
+
+print(heappop(fruits))
+
+print(fruits)
