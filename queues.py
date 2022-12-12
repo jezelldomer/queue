ba922aa80f6dc8ad4a290920ce6c24ec3053a4b5
jezelldomer <jzlldomer@gmail.com>
2022-12-12 21:33:06 +0800

    Using lifo instead of fifo

--- queues.py
+++ queues.py
@@ -22,4 +22,17 @@ print(fifo.dequeue())
 
 print(fifo.dequeue())
 
-          
+print("\n--___•___--___•___--___•___--___•___--___•___--\n")
+
+#now trying lifo instead of fifo          
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+
+print(lifo.pop())
+
+print(lifo.pop())
