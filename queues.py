5c14e8a5dca7d3f8df92e3e4a099c9d1e2475541
jezelldomer <jzlldomer@gmail.com>
2022-12-12 22:11:51 +0800

    Implementing priority queue using heap

--- queues.py
+++ queues.py
@@ -36,3 +36,34 @@ print(lifo.pop())
 print(lifo.pop())
 
 print(lifo.pop())
+
+
+
+print("\n--___•___--___•___--___•___--___•___--___•___--\n")
+
+#priority queue using heap
+from collections import deque
+from heapq import heappop, heappush
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)
+        
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue())        
