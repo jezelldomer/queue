8bfa02a1703262561c2a72f650cc8f6912724ea3
jezelldomer <jzlldomer@gmail.com>
2022-12-12 20:47:35 +0800

    printing the queue

--- queues.py
+++ queues.py
@@ -10,5 +10,16 @@ class Queue:
 
     def dequeue(self):
         return self._elements.popleft()
+        
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+print(fifo.dequeue())
+
+print(fifo.dequeue())
+
+print(fifo.dequeue())
 
           
