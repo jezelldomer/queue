b24c0c15feb33c52040e4ebb069b09ac30b73922
jezelldomer <jzlldomer@gmail.com>
2022-12-12 21:53:32 +0800

    using tuple comparison

--- heap.py
+++ heap.py
@@ -1,5 +1,5 @@
 from heapq import heappush
-
+#using heappush
 fruits = []
 heappush(fruits, "orange")
 heappush(fruits, "apple")
@@ -8,9 +8,19 @@ heappush(fruits, "banana")
 print(fruits)
 
 print("\n--___•___--___•___--___•___--___•___--___•___--\n")
-
+#using heappop
 from heapq import heappop
 
 print(heappop(fruits))
 
 print(fruits)
+
+print("\n--___•___--___•___--___•___--___•___--___•___--\n")
+#tuple comparison
+person1 = ("Jez", "Berry", 42)
+person2 = ("Jez", "Domer", 42)
+person3 = ("Jez", "Domer", 24)
+
+print(person1 < person2)
+
+print(person2 < person3)
